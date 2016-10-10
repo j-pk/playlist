20965f2246bc1c041c3ccaccb36ab0c3a69a9b74
j-pk <parker.kirby@gmail.com>
2016-10-09 21:59:44 -0400

    moved filess

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ function update_values(rowData) {
         var output = document.querySelectorAll(data_value);
         addClickHandler(output);
         output.forEach(function(element, index) {
-            if (rowData.length === 0) {
+            if (rowData === null) {
                 output[index].innerHTML = '<favorite-star/>';
             } else {
                 if (rowData[index] === true) {
