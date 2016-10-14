0a4e5b359f432f106bc071a82ecf7a704e88a499
j-pk <parker.kirby@gmail.com>
2016-10-13 20:16:23 -0400

    blah

--- server.js
+++ server.js
@@ -14,28 +14,26 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-    // Save database object from the callback for reuse.
-    db = database;
-    console.log("Database connection ready");
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-    // Initialize the app.
-    var server = app.listen(process.env.PORT || 8080, function() {
-        var port = server.address().port;
-        console.log("App now running on port", port);
-    });
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
 function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({
-        "error": message
-    });
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
 }
 
 /*  "/contacts"
@@ -43,27 +41,27 @@ function handleError(res, reason, message, code) {
  *    POST: creates a new contact
  */
 
-app.get("/favorited", function(req, res) {
-    db.collection(FAVORITED_COLLECTION).find({}).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get favorited.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
+ app.get("/favorited", function(req, res) {
+   db.collection(FAVORITED_COLLECTION).find({}).toArray(function(err, docs) {
+     if (err) {
+       handleError(res, err.message, "Failed to get favorited.");
+     } else {
+       res.status(200).json(docs);
+     }
+   });
+ });
 
 app.post("/favorited", function(req, res) {
-    var newFavorite = req.body;
-    newFavorite.createDate = new Date();
+  var newFavorite = req.body;
+  newFavorite.createDate = new Date();
 
-    db.collection(FAVORITED_COLLECTION).insertOne(newFavorite, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, "Failed to favorite song");
-        } else {
-            res.status(201).json(doc.ops[0]);
-        }
-    });
+  db.collection(FAVORITED_COLLECTION).insertOne(newFavorite, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to favorite song");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
 });
 
 /*  "/contacts/:id"
@@ -73,38 +71,35 @@ app.post("/favorited", function(req, res) {
  */
 
 app.get("/favorited/:row", function(req, res) {
-    db.collection(FAVORITED_COLLECTION).findOne({
-        row: new ObjectID(req.params.id)
-    }, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, "Failed to get favorited song");
-        } else {
-            res.status(200).json(doc);
-        }
-    });
+  db.collection(FAVORITED_COLLECTION).findOne({row: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get favorited song");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/favorited/:row", function(req, res) {
-    var
+  var update = {
+      "favorited": req.body.favorited,
+  };
 
-    db.collection(FAVORITED_COLLECTION).find({row: req.row}, {favorited: req.body.favorited}, function(err, docs) {
-            if (err) {
-                handleError(res, err.message, "Failed to update favorited song");
-            } else {
-                res.status(200).json(docs);
-            }
-        });
-    });
+  db.collection(FAVORITED_COLLECTION).updateOne({"row": req.row}, update, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update favorited song");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
 app.delete("/favorited/:id", function(req, res) {
-    db.collection(FAVORITED_COLLECTION).deleteOne({
-        _id: new ObjectID(req.params.id)
-    }, function(err, result) {
-        if (err) {
-            handleError(res, err.message, "Failed to delete favorited song");
-        } else {
-            res.status(204).end();
-        }
-    });
+  db.collection(FAVORITED_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete favorited song");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
