52fc414b016345aa57762fb7a619252976a6ebb7
j-pk <parker.kirby@gmail.com>
2016-10-16 19:03:30 -0400

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ function load_database() {
     var rowData = DataStore.getAll();
     console.log(rowData);
     update_values(rowData);
-    var blah = update_counter(rowData);
+    var blah = update_values(rowData);
     console.log(blah);
 };
 
@@ -25,6 +25,7 @@ function addClickHandler(output) {
 };
 
 function update_values(rowData) {
+  var counter = 0;
     window.onload = function() {
         var output = document.querySelectorAll(data_value);
         addClickHandler(output);
@@ -35,6 +36,7 @@ function update_values(rowData) {
                 if (rowData[index].favorited === true) {
                     output[index].innerHTML = '<favorite-star active/>';
                     output[index].setAttribute('id', 'favorited');
+                    counter++;
                 } else {
                     output[index].innerHTML = '<favorite-star/>';
                     output[index].setAttribute('id', 'notFavorited');
@@ -42,16 +44,7 @@ function update_values(rowData) {
             }
         })
     }
-};
-
-function update_counter(output) {
-  var counter = 0;
-  output.forEach(function(element, index) {
-    if (element.id === "favorited") {
-      counter++;
-    }
-  });
-  return counter;
+    return counter;
 };
 
 load_database();
