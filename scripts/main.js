d096272ab2672cd8e25c71e62922712e7e12a28f
j-pk <parker.kirby@gmail.com>
2016-10-16 15:34:38 -0400

    blah

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,7 @@ function load_database() {
 };
 
 function addClickHandler(output) {
+  var counter = 0;
     output.forEach(function(element, index) {
         element.addEventListener('click', function(event) {
             console.log(element, index);
@@ -15,18 +16,19 @@ function addClickHandler(output) {
               DataStore.update(index, true);
               element.setAttribute('id', 'notFavorited');
             } else {
+              counter += 1
               DataStore.update(index, false);
               element.setAttribute('id', 'favorited');
             }
         });
     });
+    console.log(counter);
 };
 
 function update_values(rowData) {
     window.onload = function() {
         var output = document.querySelectorAll(data_value);
         addClickHandler(output);
-        update_counter(output);
         console.log(update_counter(output));
         output.forEach(function(element, index) {
             if (rowData === undefined || rowData[index] === undefined) {
@@ -42,17 +44,6 @@ function update_values(rowData) {
             }
         })
     }
-};
-
-function update_counter(output) {
-  var counter = 0;
-  output.forEach(function(element, index) {
-    if (element.id === "favorited") {
-      counter += 1
-      console.log(element.id);
-    }
-  });
-  return counter;
 };
 
 load_database();
